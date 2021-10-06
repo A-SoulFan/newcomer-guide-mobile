699c83d0f10d8e8e666d57614d367b826ff3fb32
Sunglow <wwwugsy@qq.com>
2021-10-06 10:23:28 +0800

    <feat>: 引入Swiper组件 v5.4.5

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import less from 'less'
-
+import Swiper from 'swiper'
+import 'swiper/css/swiper.min.css'
 Vue.config.productionTip = false
 
 Vue.use(less)
+Vue.use(Swiper)
 
 new Vue({
   render: h => h(App),
